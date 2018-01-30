28b1fb4b489865d21eb2ee575fb5ad0e24fe2456
Samahara <le.chat.au.chapeau@hotmail.com>
2018-01-29 18:49:46 -0600

    redirección

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ $('#google_account').click(function (){
 function authGoogle(){
   var provider = new firebase.auth.GoogleAuthProvider();
   authentication(provider);
+  goToPage(provider);
 }
 
 function authentication(provider){
@@ -37,6 +38,28 @@ function authentication(provider){
 });
 }
 
+function goToPage(provider) {
+  firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+    window.location = "views/page.html";
+
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
 // // Sign Up
 // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 //   // Handle Errors here.
